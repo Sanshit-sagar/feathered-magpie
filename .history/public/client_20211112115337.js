2ff31b2fb7f814f432dbb878483df3c9d218a5ee
Sanshit Sagar <sasagar@ucsd.edu>
2021-11-12 14:20:18 -0800

    adding styles globally

--- .history/public/client_20211112115337.js
+++ .history/public/client_20211112115337.js
@@ -1,4 +1,3 @@
-// Establish a Socket.io connection
 const socket = io();
 // Initialize our Feathers client application through Socket.io
 // with hooks and authentication.
---
+++ .history/src/app_20211112131107.ts
@@ -0,0 +1,46 @@
+import path from 'path'
+import cors from 'cors';
+import helmet from 'helmet';
+import compression from 'compression';
+import favicon from 'serve-favicon';
+
+import express from '@feathersjs/express';
+import feathers from '@feathersjs/feathers';
+import socketio from '@feathersjs/socketio';  
+import configuration from '@feathersjs/configuration';
+
+import logger from './logger';
+import channels from './channels';
+import services from './services';
+import appHooks from './app.hooks';
+import middleware from './middleware';
+import authentication from './authentication';
+import { Application } from './declarations';
+
+const app: Application = express(feathers()); 
+
+app.configure(configuration())
+
+app.use(cors());
+app.use(helmet());
+app.use(compression())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
+app.use('/', express.static(app.get('public')));
+
+app.configure(express.rest());
+app.configure(socketio());
+
+app.configure(middleware);
+// app.configure(authentication);
+app.configure(services);
+app.configure(channels);
+
+app.use(express.notFound());
+app.use(express.errorHandler({ logger }));
+
+app.hooks(appHooks);
+
+export default app; 
+
